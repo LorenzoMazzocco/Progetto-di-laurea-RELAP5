d5c0a81c1cd2cedcd3798c7c80b62393dd1e8527
Marcooo201 <103608570+Marcooo201@users.noreply.github.com>
2022-05-20 15:18:14 +0200

    Update pressure_energy.m

--- utils/other/pressure_energy.m
+++ utils/other/pressure_energy.m
@@ -4,9 +4,11 @@ function [pressure_energy] = pressure_energy (pressure)
     % restituisco un vettore colonna di m-righe rappresentante l'energia di
     % pressione di tutto il fluido contenuto nella pipe per ogni dt
 
+    % la pressione è estratta in bar
+
     A = 8.79e-5;                        % [m^2]
     height = 0.07752;                   % [m]
     single_volume = A*height;           % [m^3]
 
-    pressure_energy = single_volume .* sum(pressure,2);   % [J]
+    pressure_energy = single_volume .* sum(pressure,2) .* 10^5;   % [J]
 end
